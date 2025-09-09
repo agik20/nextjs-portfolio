4c6a29f244df9bafd41c56cc7a8bd38d0b9735cf
heimy <argi8304@gmail.com>
2025-09-09 09:53:21 +0700

    updated about and navbar

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,4 @@
-import { assets, infoList } from '@/assets/assets'
+import { assets, infoList, toolsData, tool } from '@/assets/assets'
 import Image from 'next/image'
 import React, { useRef } from 'react'
 
@@ -8,7 +8,7 @@ const About = () => {
       <h4 className='text-center mb-2 text-lg font-crimson'>Introduction</h4>
       <h2 className='text-center text-5xl font-crimson'>About Me</h2>
 
-      <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+      <div className='flex w-full flex-col px-[12%] lg:flex-row items-center gap-20 my-20'>
         <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
             <Image src={assets.user_image} alt='user' className='w-full rounded-3xl'></Image>
         </div>
@@ -17,12 +17,23 @@ const About = () => {
 
             <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                 {infoList.map(({icon, iconDark, title, description}, index)=>(
-                    <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[--color-dark-hover] hover:-translate-y-1 duration-500' key={index}>
+                    <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-gray-200 hover:-translate-y-1 duration-500 hover:4px 4px 0 #000' key={index}>
                         <Image src={icon} alt={title} className='w-7 mt-3'></Image>
                         <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
                         <p className='text-gray-600 text-sm'>{description}</p>
                     </li>
                 ))}
+            </ul>
+
+            <h4 className='my-6 text-gray-700 font-crimson'>
+              Tools i use
+            </h4>
+            <ul className='flex items-center gap-3 sm:gap-5'>
+              {toolsData.map((tool, index)=>(
+                <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' key={index}>
+                  <Image src={tool} alt='Tool' className='w-5 sm:w-7'></Image>
+                </li>
+              ))}
             </ul>
         </div>
       </div>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
 const Navbar = () => {
 
+    const [isScroll, setIsScroll] = useState(false)
     const sideMenuRef = useRef();
 
     const openMenu = ()=>{
@@ -13,17 +14,27 @@ const Navbar = () => {
         sideMenuRef.current.style.transform = 'translate(16rem)'
     }    
 
+    useEffect(()=>{
+      window.addEventListener('scroll', ()=>{
+        if(scrollY > 50){
+          setIsScroll(true)
+        }else{
+          setIsScroll(false)
+        }
+      })
+    },[])
+
 
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'> 
         <Image src={assets.header_bg_color} alt='' className='w-full'></Image>
     </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex item-center justify-between z-50">
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
         <a href="#top">
           <Image src={assets.logo} alt="Logo" className="w-28 cursor-pointer mr-14"/>
         </a>
-        <ul className='hidden md:flex item-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-20"}`}>
           <li><a className='font-crimson' href="#top">Home</a></li>
           <li><a className='font-crimson' href="#about">About</a></li>
           <li><a className='font-crimson' href="#skills">Skills</a></li>
@@ -31,13 +42,13 @@ const Navbar = () => {
           <li><a className='font-crimson' href="#contact">Contact</a></li>
         </ul>
 
-        <div className='flex item-center gap-4'>
+        <div className='flex items-center gap-4'>
 
             <button>
                 <Image src={assets.moon_icon} alt='' className='w-6 cursor-pointer'></Image>
             </button>
 
-          <a href="contact" className='hidden lg:flex item-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-crimson'>Contact <Image src={assets.arrow_icon} alt="Arrow icon" className="w-3"/></a>
+          <a href="contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-crimson'>Contact <Image src={assets.arrow_icon} alt="Arrow icon" className="w-3"/></a>
 
           <button className='block md:hidden ml-3' onClick={openMenu}>
             <Image src={assets.menu_black} alt='' className='w-6 cursor-pointer'></Image>
