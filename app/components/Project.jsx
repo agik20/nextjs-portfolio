4e5da7796377a244ac1ae4df8c3e98f07c21ad70
heimy <argi8304@gmail.com>
2025-09-11 07:56:01 +0700

    added motion

--- app/components/Project.jsx
+++ app/components/Project.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import Image from 'next/image'
 import { assets, workData } from '@/assets/assets'
+import { motion, scale } from 'motion/react'
 
 const Project = () => {
   return (
-    <div id="project" className='w-full px-[12%] py-10 scroll-mt-20'>
-        <h4 className='text-center mb-2 text-lg font-crimson'>My Portfolio</h4>
-        <h2 className='text-center text-5xl font-crimson'>My Latest Work</h2>
-        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-crimson'>I specialize in data science, from cleaning and analyzing data to building and tuning machine learning models. I focus on turning raw data into clear insights and deployable solutions that solve real problems and support intelligent decision-making.</p>
+    <motion.div 
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1}}     
+    id="project" className='w-full px-[12%] py-10 scroll-mt-20'>
+        <motion.h4 
+        initial={{y: -20, opacity: 0}}
+        whileInView={{y: 0, opacity: 1}}
+        transition={{delay: 0.3, duration: 0.5}}                  
+        className='text-center mb-2 text-lg font-crimson'>My Portfolio</motion.h4>
+        <motion.h2 
+        initial={{y: -20, opacity: 0}}
+        whileInView={{y: 0, opacity: 1}}
+        transition={{delay: 0.5, duration: 0.5}}          
+        className='text-center text-5xl font-crimson'>My Latest Work</motion.h2>
+        <motion.p 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{delay: 0.7, duration: 0.5}}          
+        className='text-center max-w-2xl mx-auto mt-5 mb-12 font-crimson'>I specialize in data science, from cleaning and analyzing data to building and tuning machine learning models. I focus on turning raw data into clear insights and deployable solutions that solve real problems and support intelligent decision-making.</motion.p>
 
-        <div className='grid grid-cols-auto my-10 gap-5'>
+        <motion.div 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{delay: 0.9, duration: 0.6}}          
+        className='grid grid-cols-auto my-10 gap-5'>
             {workData.map((project, index)=>(
-                <div className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' key={index} style={{backgroundImage: `url(${project.bgImage})`}}>
+                <motion.div 
+                whileInView={{scale: 1}}
+                transition={{duration: 0.3}}                  
+                className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' key={index} style={{backgroundImage: `url(${project.bgImage})`}}>
                     <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
                         <div>
                             <h2 className='font-semibold'>{project.title}</h2>
@@ -21,13 +45,17 @@ const Project = () => {
                             <Image src={assets.send_icon} alt='send icon' className='w-5'></Image>
                         </div>                        
                     </div>
-                </div>
+                </motion.div>
             ))}
-        </div>
-        <a href="" className='w-max flex items-center jusrify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full mx-auto py-3 px-10  my-20 hover:bg-light-hover duration-500'>
+        </motion.div>
+        <motion.a 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{delay: 1.1, duration: 0.5}}          
+        href="" className='w-max flex items-center jusrify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full mx-auto py-3 px-10  my-20 hover:bg-light-hover duration-500'>
             show more <Image src={assets.right_arrow_bold} alt='right arrow' className='w-4'></Image>
-        </a>
-    </div>
+        </motion.a>
+    </motion.div>
   )
 }
 
--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -1,27 +1,50 @@
 import React from 'react'
 import Image from 'next/image'
 import { assets, serviceData } from '@/assets/assets'
+import { motion, scale } from 'motion/react'
 
 const Skills = () => {
   return (
-    <div id="skills" className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4 className='text-center mb-2 text-lg font-crimson'>Skills</h4>
-      <h2 className='text-center text-5xl font-crimson'>My Expertise</h2>
-      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-crimson'>I specialize in data science, from cleaning and analyzing data to building and tuning machine learning models. I focus on turning raw data into clear insights and deployable solutions that solve real problems and support intelligent decision-making.</p>
+    <motion.div 
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1}}
+    id="skills" className='w-full px-[12%] py-10 scroll-mt-20'>
+      <motion.h4 
+      initial={{y: -20, opacity: 0}}
+      whileInView={{y: 0, opacity: 1}}
+      transition={{delay: 0.3, duration: 0.5}}      
+      className='text-center mb-2 text-lg font-crimson'>Skills</motion.h4>
+      <motion.h2 
+      initial={{y: -20, opacity: 0}}
+      whileInView={{y: 0, opacity: 1}}
+      transition={{delay: 0.3, duration: 0.5}}       
+      className='text-center text-5xl font-crimson'>My Expertise</motion.h2>
+      <motion.p 
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1}}
+      transition={{delay: 0.7, duration: 0.5}}        
+      className='text-center max-w-2xl mx-auto mt-5 mb-12 font-crimson'>I specialize in data science, from cleaning and analyzing data to building and tuning machine learning models. I focus on turning raw data into clear insights and deployable solutions that solve real problems and support intelligent decision-making.</motion.p>
 
-      <div className='grid grid-cols-auto gap-6 my-10'>
+      <motion.div 
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1}}
+      transition={{delay: 0.9, duration: 0.6}}       
+      className='grid grid-cols-auto gap-6 my-10'>
         {serviceData.map(({icon, title, description, link}, index)=>(
-            <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500'>
+            <motion.div 
+            whileInView={{scale: 1}}           
+            key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500'>
                 <Image src={icon} alt='' className='w-10'></Image>
                 <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
                 <p className='text-sm text-gray-600 leading-5'>{description}</p>
                 <a href={link} className='flex items-center gap-2 text-sm mt-5'>
                     Read more <Image src={assets.right_arrow} alt='' className='w-4'></Image>
                 </a>
-            </div>
+            </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 
